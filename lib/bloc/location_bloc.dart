b97d3965765da50cbad56f64dcf447afe988dc20
Ioana Joj <ioanna.gheorghiu@gmail.com>
2020-04-05 18:11:45 +0300

    Location subscribe and unsubscribe

--- lib/bloc/location_bloc.dart
+++ lib/bloc/location_bloc.dart
@@ -1,12 +1,14 @@
+import 'dart:async';
+import 'dart:developer' as developer;
 import 'package:conquerpeaksfe/bloc/bloc.dart';
 import 'package:bloc/bloc.dart';
 import 'package:geolocator/geolocator.dart';
-
 import 'location.dart';
 
 class LocationBloc extends Bloc<LocationEvent, LocationState> {
 
   Geolocator locationAPI = Geolocator();
+  static bool _isCancelled = false;
 
   @override
   LocationState get initialState => LocationInitial();
@@ -15,6 +17,7 @@ class LocationBloc extends Bloc<LocationEvent, LocationState> {
   Stream<LocationState> mapEventToState(LocationEvent event) async* {
     yield LocationLoading();
     if (event is GetLocation) {
+      developer.log('get location', name: 'conquer.peaks.app');
       try {
         final location = await fetchLocation();
         yield LocationLoaded(location);
@@ -22,10 +25,20 @@ class LocationBloc extends Bloc<LocationEvent, LocationState> {
         yield LocationError("Some error occurred :(");
       }
     } else if (event is ListenLocation) {
+      _isCancelled = false;
       var locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
-      await for (final position in locationAPI.getPositionStream(locationOptions)) {
-
-      }
+      developer.log('listen for location', name: 'conquer.peaks.app');
+      yield* locationAPI.getPositionStream(locationOptions)
+          .asyncMap((position) => LocationLoaded(
+          GeoCoordinates(
+              latitude: position.latitude,
+              longitude: position.longitude,
+              altitude: position.altitude)))
+          .takeWhile((position) {return !_isCancelled;});
+    } else if (event is CancelListenLocation) {
+      developer.log('cancel listen location event received', name: 'conquer.peaks.app');
+      _isCancelled = true;
+      yield LocationInitial();
     }
   }
 
